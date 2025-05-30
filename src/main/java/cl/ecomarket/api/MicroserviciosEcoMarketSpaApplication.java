7173a1acd44458a36b6262c2f3c9182002301711
Naglock <iv.huentemilla@duocuc.cl>
2025-05-30 15:57:25 -0400

    Proyecto base cargado

---
+++ src/main/java/cl/ecomarket/api/MicroserviciosEcoMarketSpaApplication.java
@@ -0,0 +1,13 @@
+package cl.ecomarket.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosEcoMarketSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosEcoMarketSpaApplication.class, args);
+	}
+
+}
