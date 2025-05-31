bbc46c711cad584a2cbe4dfa1abc33c0cc8294d0
Naglock <iv.huentemilla@duocuc.cl>
2025-05-31 11:07:27 -0400

    Se crea usuarioRepository con una funcion personalizada para encontrar por Correo a un usuario

---
+++ src/main/java/cl/ecomarket/api/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package cl.ecomarket.api.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import cl.ecomarket.api.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    List<Usuario> findByCorreo(String correo);
+
+}
