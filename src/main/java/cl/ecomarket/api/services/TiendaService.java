5ab02e45b8cda231faa887f5ee6b1c1976da3ae2
Naglock <iv.huentemilla@duocuc.cl>
2025-06-01 23:19:09 -0400

    Correccion minima de cosas que no se utilizaban

--- src/main/java/cl/ecomarket/api/services/TiendaService.java
+++ src/main/java/cl/ecomarket/api/services/TiendaService.java
@@ -1,8 +1,6 @@
 package cl.ecomarket.api.services;
 
-import java.util.List;
-import java.util.Optional;
-
+import java.util.List;  
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
