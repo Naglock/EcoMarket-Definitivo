cb230b81c90e2e595c225bd96ef5ea4625198ede
Naglock <iv.huentemilla@duocuc.cl>
2025-06-30 20:03:51 -0400

    modified:   src/main/java/cl/ecomarket/api/controller/ProductoController.java

--- src/main/java/cl/ecomarket/api/controller/ProductoController.java
+++ src/main/java/cl/ecomarket/api/controller/ProductoController.java
@@ -4,13 +4,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import cl.ecomarket.api.model.Producto;
 import cl.ecomarket.api.services.ProductoService;
 
-@Service
 @RestController
 @RequestMapping("/api/v1/productos")
 public class ProductoController {
